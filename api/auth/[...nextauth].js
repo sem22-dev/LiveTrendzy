136e610ff367d6ec3668f5183038eb006b44af67
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-08-29 13:04:31 +0530

    silly update

--- api/auth/[...nextauth].js
+++ api/auth/[...nextauth].js
@@ -1,32 +1,44 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
-import { supabase } from "../../utils/supabase"; // Import your Supabase client
+import { SupabaseAdapter } from "@auth/supabase-adapter";
+import jwt from "jsonwebtoken";
 
 export default NextAuth({
   providers: [
-    Providers.Credentials({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        const { email, password } = credentials;
-        const { user, error } = await supabase.auth.signIn({
-          email,
-          password,
-        });
-
-        if (error) {
-          throw new Error(error.message);
-        }
-
-        return user;
-      },
-    }),
+    // Add your provider configurations here
+    // Example:
+    // {
+    //   id: "google",
+    //   name: "Google",
+    //   type: "oauth",
+    //   version: "2.0",
+    //   scope: "profile email",
+    //   params: { grant_type: "authorization_code" },
+    //   accessTokenUrl: "https://accounts.google.com/o/oauth2/token",
+    //   requestTokenUrl: "https://accounts.google.com/o/oauth2/auth",
+    //   authorizationUrl:
+    //     "https://accounts.google.com/o/oauth2/auth?response_type=code&prompt=consent",
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // },
   ],
-  session: {
-    jwt: true,
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  }),
+  callbacks: {
+    async session({ session, user }) {
+      const signingSecret = process.env.SUPABASE_JWT_SECRET;
+      if (signingSecret) {
+        const payload = {
+          aud: "authenticated",
+          exp: Math.floor(new Date(session.expires).getTime() / 1000),
+          sub: user.id,
+          email: user.email,
+          role: "authenticated",
+        };
+        session.supabaseAccessToken = jwt.sign(payload, signingSecret);
+      }
+      return session;
+    },
   },
-  callbacks: {},
 });
